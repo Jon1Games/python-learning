874b55e0dc72fccc882ae1fcc1aa6da874abbb5e
Jon1Games <118659471+Jon1Games@users.noreply.github.com>
2025-09-30 14:50:38 +0200

    correct import and naming

--- mcpi/wall.py
+++ mcpi/wall.py
@@ -1,17 +1,18 @@
-from mcpi import World
+from mcpi.minecraft import Minecraft
+from mcpi import block
 
 class Wall:
-    def __init__(self, pos: tuple, bw: World, height: int = 5, width: int = 6, rotated: bool = False, material_id: str = "minecraft:stone"):
+    def __init__(self, pos: tuple, mc: Minecraft, height: int = 5, width: int = 6, rotated: bool = False, material_id: int = block.STONE.id):
         self.height = height
         self.width = width
         self.pos = pos
         self.rotated = rotated
         self.material_id = material_id
-        self.bw = bw
-        
+        self.mc = mc
+
     def build(self):
         x, y, z = self.pos
         if self.rotated:
-            self.bw.setBlocks(x, y, z, x + self.width - 1, y + self.height - 1, z, self.material_id)
+            self.mc.setBlocks(x, y, z, x + self.width - 1, y + self.height - 1, z, self.material_id)
         else:
-            self.bw.setBlocks(x, y, z, x, y + self.height - 1, z + self.width - 1, self.material_id)
+            self.mc.setBlocks(x, y, z, x, y + self.height - 1, z + self.width - 1, self.material_id)
